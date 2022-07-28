be61fb9597376e0419501d28a06556cebd3708c6
loretana7 <loretana7@gmail.com>
2022-07-28 06:52:30 -0300

    probandoSubirLaBaseDeDatos

---
+++ portfolio-prueba.sql
@@ -0,0 +1,3 @@
+select * from educacion;
+INSERT INTO `portfoliobd`.`persona` (`id`, `nombre`, `apellido`, `email`, `descripcion`, `domicilio`, `fechaNac`, `habilitada`) 
+VALUES (`3`, `yanina`, `medina`, `blabla@gmail`, `otro ser libre`, `av.castro 8282`, `1990-11-11`, `1`);
